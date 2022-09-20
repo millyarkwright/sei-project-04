39c6e0202f12ae3e1fd73984e15e36aa4dd8b87f
Milly Arkwright <millyarkwright@millys-air.home>
2022-09-20 14:27:28 +0100

    backslashed added to app urls in the backend

--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
   path('', UserListView.as_view()), 
   path('register/', RegisterView.as_view()),
   path('login/', LoginView.as_view()),
-  path('profile/<username>', UserProfileView.as_view()), 
+  path('profile/<username>/', UserProfileView.as_view()), 
   path('profile/', UserProfilePrivateView.as_view()), 
   path('bookmarks/', BookmarkedListView.as_view()), 
-  path('bookmarked/<int:pk>', BookmarkedView.as_view()), 
+  path('bookmarked/<int:pk>/', BookmarkedView.as_view()), 
   path('tested/', TestedListView.as_view()),
-  path('tested/<int:pk>', TestedView.as_view()),
+  path('tested/<int:pk>/', TestedView.as_view()),
 ]
 
--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ReviewView
 
 urlpatterns = [
-    path('<int:pk>', ReviewView.as_view()),
+    path('<int:pk>/', ReviewView.as_view()),
 ]
 
