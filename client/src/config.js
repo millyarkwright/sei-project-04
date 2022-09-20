8170b0957d3e5a7bd6fea411db40f61c665fb1d6
fasaic <fasaic.113@gmail.com>
2022-09-20 16:43:19 +0100

    modified configurations for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,3 @@
 const DEV_API_URL = "/api";
 const PROD_API_URL = process.env.REACT_APP_API_URL;
-export const API_URL =
-  process.env.NODE_ENV === "production" ? PROD_API_URL : DEV_API_URL;
+export const API_URL = "/api";
