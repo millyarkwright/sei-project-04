94d5817cf2a1e35d5deecfa5215fb3d7963101b8
Milly Arkwright <milly.arkwright@gmail.com>
2023-01-03 17:19:04 +0000

    static files

--- project/settings.py
+++ project/settings.py
@@ -61,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware', -> for fly.io
 ]
 
 ROOT_URLCONF = 'project.urls'
